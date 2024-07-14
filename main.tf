7c7107e92a1e974dd77242c41ef2cd1420e44ad0
YavorMarkov <yavormaxpower@gmail.com>
2024-07-14 16:30:03 +0300

    edit some code

--- main.tf
+++ main.tf
@@ -1,8 +1,11 @@
 module "compute_instance" {
   source = "./modules/compute"
 
+
+  region      = var.region       
+  project_id  = var.project_id   
   zone                = "us-central1-f"
   image               = "debian-cloud/debian-11"
-  startup_script_url  = "https://raw.githubusercontent.com/YavorMarkov/test-2/main/startup.sh"
+  startup_script_url  = "https://raw.githubusercontent.com/YavorMarkov/terraform-gcp-modules/main/startup.sh"
   my_ip               = "YOUR_PUBLIC_IP"
 }
--- modules/compute/variables.tf
+++ modules/compute/variables.tf
@@ -1,3 +1,4 @@
+
 variable "region" {
   description = "The region where the resources will be created"
   type        = string
