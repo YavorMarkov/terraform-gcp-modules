75da87f5d20d731da29b0a6ade265eff2bf18c01
YavorMarkov <yavormaxpower@gmail.com>
2024-07-14 19:00:43 +0300

    *

--- modules/compute/firewall.tf
+++ modules/compute/firewall.tf
@@ -7,6 +7,6 @@ resource "google_compute_firewall" "secure_http" {
     ports    = ["80", "443"]
   }
 
-  source_ranges = [format("%s", var.my_ip)]
+  source_ranges = [var.my_ip]
   target_tags   = ["web"]
 }
