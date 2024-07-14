614d8f3810fcef760c087a7756d5e4c6b9c2177e
YavorMarkov <yavormaxpower@gmail.com>
2024-07-14 17:16:48 +0300

    *

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
- provider "google" {
+provider "google" {
   credentials = file(var.GOOGLE_CREDENTIALS)
   project     = var.project_id
   region      = var.region
@@ -6,15 +6,10 @@
 
 module "compute_instance" {
   source              = "./modules/compute"
+  project_id          = var.project_id
+  region              = var.region
   zone                = var.zone
   image               = var.image
   startup_script_url  = var.startup_script_url
   my_ip               = var.my_ip
 }
-  
-
-
-
-
-  startup_script_url  = "https://raw.githubusercontent.com/YavorMarkov/terraform-gcp-modules/main/scripts/setup_nginx.sh"
-  
--- variables.tf
+++ variables.tf
@@ -1,15 +1,33 @@
+
+variable "region" {
+  description = "The region where the resources will be created"
+  type        = string
+}
+
 variable "project_id" {
   description = "The Google Cloud project ID"
   type        = string
 }
 
-variable "GOOGLE_CREDENTIALS" {
-  description = "The JSON credentials for Google Cloud"
+
+
+variable "zone" {
+  description = "The zone to deploy resources in"
   type        = string
-  sensitive   = true
 }
 
-variable "region" {
-  description = "The region to deploy resources in"
+variable "image" {
+  description = "The image to use for the boot disk"
+  type        = string
+}
+
+variable "startup_script_url" {
+  description = "URL to the startup script"
+  type        = string
+  default     = "https://raw.githubusercontent.com/YavorMarkov/terraform-gcp-modules/main/scripts/setup_nginx.sh"
+}
+
+variable "my_ip" {
+  description = "Your public IP address for restricted access"
   type        = string
 }
