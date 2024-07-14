3144b159a1389d51e2bc388db0c4e2fd555feffb
YavorMarkov <yavormaxpower@gmail.com>
2024-07-14 16:45:58 +0300

    *

--- main.tf
+++ main.tf
@@ -2,14 +2,15 @@ module "compute_instance" {
   source = "./modules/compute"
 
 
- provider "google" {
-  region      = var.region       
-  project_id  = var.project_id
-  credentials  = file(var.GOOGLE_CREDENTIALS)
-}
    
   zone                = "us-central1-f"
   image               = "debian-cloud/debian-11"
   startup_script_url  = "https://raw.githubusercontent.com/YavorMarkov/terraform-gcp-modules/main/scripts/setup_nginx.sh"
   my_ip               = "YOUR_PUBLIC_IP"
+}
+
+provider "google" {
+  credentials = file(var.GOOGLE_CREDENTIALS)
+  project     = var.project_id
+  region      = var.region
 }
