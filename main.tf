9c26007d2832e7be4e8bd3e2c74c19d11fd88cb7
YavorMarkov <yavormaxpower@gmail.com>
2024-07-14 16:33:37 +0300

    edit *

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ module "compute_instance" {
   project_id  = var.project_id   
   zone                = "us-central1-f"
   image               = "debian-cloud/debian-11"
-  startup_script_url  = "https://raw.githubusercontent.com/YavorMarkov/terraform-gcp-modules/main/startup.sh"
+  startup_script_url  = "https://raw.githubusercontent.com/YavorMarkov/terraform-gcp-modules/main/scripts/setup_nginx.sh"
   my_ip               = "YOUR_PUBLIC_IP"
 }
