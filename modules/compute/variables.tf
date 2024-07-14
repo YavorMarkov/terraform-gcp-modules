b27c8c82b8091fd23526201b6dad4756764d9266
YavorMarkov <yavormaxpower@gmail.com>
2024-07-14 16:22:05 +0300

    add variables

--- modules/compute/variables.tf
+++ modules/compute/variables.tf
@@ -1,3 +1,15 @@
+variable "region" {
+  description = "The region where the resources will be created"
+  type        = string
+}
+
+variable "project_id" {
+  description = "The Google Cloud project ID"
+  type        = string
+}
+
+
+
 variable "zone" {
   description = "The zone to deploy resources in"
   type        = string
